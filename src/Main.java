5f603c3c9132e85747031a66613e2e955c2ae8fe
luso04 <04joaosilva@gmail.com>
2024-10-01 15:22:57 +0200

    Login and test of git - 40381

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+      
+        System.out.println("Please insert your user id to enter");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] ids = new int[50000];
+
+        for (int i = 1; i <= 49999; i++) {
+            ids[i] = i;
         }
-        //jdhgkjfdhikjgfdhefsf
-        //hjsagfjhagfjha
+
+        Scanner scanner = new Scanner(System.in);
+        int inputId = scanner.nextInt();
+
+        boolean isValid = false;
+        for (int id : ids) {
+            if (id == inputId) {
+                isValid = true;
+                break; // Exit the loop as soon as a match is found
+            }
+        }
+
+        if (isValid) {
+            System.out.println("Login successful");
+        } else {
+            System.out.println("Login failed");
+        }
+
+        scanner.close();
     }
 }
