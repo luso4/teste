2d7e1283a2131e5413a74273690306c5d67f6e65
Y********* <y*********@gmail.com>
2024-10-04 00:23:00 +0200

    Create a class Student that saves the information of the id, time and credit

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,70 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
       
         System.out.println("Please insert your user id to enter");
 
-        int[] ids = new int[50000];
+        List<Student> students = new ArrayList<>();
 
         for (int i = 1; i <= 49999; i++) {
-            ids[i] = i;
+            long time = System.currentTimeMillis();  
+            double credit = Math.random() * 100;
+            double timeCredit = time/15;
+            timeCredit = timeCredit*0.1;
+            timeCredit = credit-timeCredit;
+            timeCredit = timeCredit/0.1;
+            timeCredit = timeCredit*15;
+            
+            students.add(new Student(i, time, credit, timeCredit));
         }
 
         Scanner scanner = new Scanner(System.in);
         int inputId = scanner.nextInt();
 
+        int currentId;
+        long currentTime;
+        double currentCredit;
+        double currentTimeCredit = 0;
+
         boolean isValid = false;
-        for (int id : ids) {
-            if (id == inputId) {
+        for (Student student : students) {
+            if (student.id == inputId) {
                 isValid = true;
-                break; // Exit the loop as soon as a match is found
+                currentId = student.id;
+                currentTime = student.time;
+                currentCredit = student.credit;
+                currentTimeCredit = student.timeCredit;
+                System.out.println("Login successful");
+                System.out.println("Time: " + currentTime);
+                System.out.println("Price: " + currentCredit);
+                System.out.println("Time Avaliable: " + currentTimeCredit);
             }
         }
-
-        if (isValid) {
-            System.out.println("Login successful");
-        } else {
+        if (isValid == false)
+        {
             System.out.println("Login failed");
         }
-
         scanner.close();
+
+        
+        //Add more credit to the student so he can pay 
+        /*
+        if (currentTimeCredit<0)
+        {
+            System.out.println("You currently have less credit that you need to pay do you wish to add credit");
+            System.out.println("Y/N");
+        }
+
+        Scanner scannerMoreCredit = new Scanner(System.in);
+        String inputMoreCredut = scannerMoreCredit.nextLine();
+
+        if(inputMoreCredut.equals("Y"))
+        {
+            System.out.println("How much credit do you want to deposit");
+        }
+       */
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    //Class with the information of the student
+    int id;
+    long time;
+    double credit;
+    double timeCredit; // Time that the student has avaliable in the park
+
+
+    public Student (int id, long time, double credit, double timeCredit )
+    {
+        this.id = id;
+        this.time = time;
+        this.credit = credit;
+        this.timeCredit = timeCredit;
+    }
+    
+}
