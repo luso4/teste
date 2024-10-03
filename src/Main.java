7fddf6b5bccdbaaed8cfccdd22dcfc9c39823ae5
Y********* <y*********@gmail.com>
2024-10-04 01:31:37 +0200

    Created a option to add the car to the park

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
       
         System.out.println("Please insert your user id to enter");
 
+        //Create the data of the student
         List<Student> students = new ArrayList<>();
 
         for (int i = 1; i <= 49999; i++) {
@@ -17,8 +18,8 @@ public class Main {
             timeCredit = credit-timeCredit;
             timeCredit = timeCredit/0.1;
             timeCredit = timeCredit*15;
-            
-            students.add(new Student(i, time, credit, timeCredit));
+            int carPark = (int) (Math.random() * 2);
+            students.add(new Student(i, time, credit, timeCredit, carPark));
         }
 
         Scanner scanner = new Scanner(System.in);
@@ -27,7 +28,8 @@ public class Main {
         int currentId;
         long currentTime;
         double currentCredit;
-        double currentTimeCredit = 0;
+        double currentTimeCredit;
+        int currentCarPark = 0;
 
         boolean isValid = false;
         for (Student student : students) {
@@ -37,19 +39,47 @@ public class Main {
                 currentTime = student.time;
                 currentCredit = student.credit;
                 currentTimeCredit = student.timeCredit;
+                currentCarPark = student.carPark;
                 System.out.println("Login successful");
                 System.out.println("Time: " + currentTime);
                 System.out.println("Price: " + currentCredit);
                 System.out.println("Time Avaliable: " + currentTimeCredit);
+                System.out.println("Car Park: " + (currentCarPark == 0 ? "No" : "Yes"));
             }
         }
         if (isValid == false)
         {
             System.out.println("Login failed");
+            scanner.close();
         }
-        scanner.close();
+       
 
+        //Add the car of the student to the park
         
+       
+        if (currentCarPark == 0)
+        {
+            
+            System.out.println("Do you wish to park your car?");
+            System.out.println("Y/N");
+            String inputCarPark = scanner.next();
+            
+            if (inputCarPark.equalsIgnoreCase("Y"))
+            {
+                currentCarPark = 1;
+                System.out.println("The Car was park");
+            }
+            else
+            {
+                currentCarPark = 0;
+                System.out.println("OK");
+            }
+        }
+        else
+        {
+            System.out.println("All Done For Now");
+        }
+
         //Add more credit to the student so he can pay 
         /*
         if (currentTimeCredit<0)
--- src/Student.java
+++ src/Student.java
@@ -4,14 +4,15 @@ public class Student {
     long time;
     double credit;
     double timeCredit; // Time that the student has avaliable in the park
+    int carPark; // TO check if the car is inside 
 
-
-    public Student (int id, long time, double credit, double timeCredit )
+    public Student (int id, long time, double credit, double timeCredit, int carPark )
     {
         this.id = id;
         this.time = time;
         this.credit = credit;
         this.timeCredit = timeCredit;
+        this.carPark = carPark;
     }
     
 }
